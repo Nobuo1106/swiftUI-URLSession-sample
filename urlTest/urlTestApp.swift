cfe4d5403380cb60669ae6d06ab2c4b86d2457d6
五十嵐伸雄 <gonnbe1106@gmail.com>
2023-10-14 15:05:14 +0900

    Initial Commit

---
+++ urlTest/urlTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  urlTestApp.swift
+//  urlTest
+//
+//  Created by 五十嵐伸雄 on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct urlTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
